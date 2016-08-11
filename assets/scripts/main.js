dac9420cab5ba9c32b93b2a042d56ad780cd0a9f
Hudson Nicoletti <hudson19962009@gmail.com>
2016-08-11 16:25:21 -0300

    11-08

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,9 @@
-(function () {
-});
+(function ($) {
+    function flip() {
+        var words1 = $(".word-flip-1"), words2 = $(".word-flip-2"), words3 = $(".word-flip-3"), words4 = $(".word-flip-4");
+        words1.forEach(function (i, e) {
+            console.log(i);
+        });
+    }
+    setInterval(flip(), 3500);
+})(jQuery);
--- assets/scripts/typescript/main.ts
+++ assets/scripts/typescript/main.ts
@@ -1,3 +1,17 @@
-() => {
-  
-}
+(($) => {
+
+  function flip()
+  {
+    let words1 = $(".word-flip-1"),
+        words2 = $(".word-flip-2"),
+        words3 = $(".word-flip-3"),
+        words4 = $(".word-flip-4")
+
+    words1.each((i,e)=>{
+      console.log(i)
+    })
+  }
+
+  setInterval( flip() , 3500 )
+
+})(jQuery)
