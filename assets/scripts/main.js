533c5184774dcbc7eb8dede262d01cd8d8c67f47
Hudson Nicoletti <hudson19962009@gmail.com>
2016-08-12 15:16:04 -0300

    animations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,11 @@
 (function ($) {
-    function flip() {
-        var words1 = $(".word-flip-1"), words2 = $(".word-flip-2"), words3 = $(".word-flip-3"), words4 = $(".word-flip-4");
-        words1.forEach(function (i, e) {
-            console.log(i);
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $(window).ready(function () {
+        $('.brain-2 .brain').addClass("animate").one(animationEnd, function () {
+            $('.brain-3 .brain').addClass("animate");
+            $('.brain-3 .brain:last').one(animationEnd, function () {
+                $('.piggy .bar').addClass("animate");
+            });
         });
-    }
-    setInterval(flip(), 3500);
+    });
 })(jQuery);
--- assets/scripts/typescript/main.ts
+++ assets/scripts/typescript/main.ts
@@ -1,17 +1,17 @@
-(($) => {
+declare const jQuery
+(($)=>{
 
-  function flip()
-  {
-    let words1 = $(".word-flip-1"),
-        words2 = $(".word-flip-2"),
-        words3 = $(".word-flip-3"),
-        words4 = $(".word-flip-4")
+  const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
 
-    words1.each((i,e)=>{
-      console.log(i)
+  $(window).ready(()=>{
+
+    $('.brain-2 .brain').addClass("animate").one(animationEnd,()=>{
+      $('.brain-3 .brain').addClass("animate")
+      $('.brain-3 .brain:last').one(animationEnd,()=>{
+        $('.piggy .bar').addClass("animate")
+      })
     })
-  }
 
-  setInterval( flip() , 3500 )
+  })
 
 })(jQuery)
