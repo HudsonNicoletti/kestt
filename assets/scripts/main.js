86c562bcf96b09afffec807080e16dc746a21d0f
Hudson Nicoletti <hudson19962009@gmail.com>
2016-08-29 10:53:15 -0300

    fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -28,6 +28,7 @@
                 squeeze: 124,
                 animate: 250
             });
+            
             $("#carousel .slide a").on("click", function () {
                 $(".navigate-left").trigger("click");
                 return false;
