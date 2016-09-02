b916866e4d5e697a09c398d7a9e395fbb8f77949
Hudson Nicoletti <hudson19962009@gmail.com>
2016-09-02 13:09:27 -0300

    texts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,9 +12,9 @@
             $("nav ul").toggleClass("active");
         });
         $("section.info ul.about li").on("click", function () {
-            var title = $(this).find("a").html(), text = $(this).find("span.text").text(), view = $(".view"), vtitle = view.find(".title > h1"), vtext = view.find(".text > p");
+            var title = $(this).find("a").html(), text = $(this).find("span.text").html(), view = $(".view"), vtitle = view.find(".title > h1"), vtext = view.find(".text > p");
             vtitle.html(title);
-            vtext.text(text);
+            vtext.html(text);
             $(this).siblings(".active").removeClass("active");
             $(this).addClass("active");
             return false;
@@ -28,7 +28,6 @@
                 squeeze: 124,
                 animate: 250
             });
-            
             $("#carousel .slide a").on("click", function () {
                 $(".navigate-left").trigger("click");
                 return false;
--- assets/scripts/typescript/main.ts
+++ assets/scripts/typescript/main.ts
@@ -20,13 +20,13 @@
 
     $("section.info ul.about li").on("click",function(){
       let title = $(this).find("a").html(),
-          text = $(this).find("span.text").text(),
+          text = $(this).find("span.text").html(),
           view = $(".view"),
           vtitle = view.find(".title > h1"),
           vtext = view.find(".text > p")
 
       vtitle.html(title)
-      vtext.text(text)
+      vtext.html(text)
 
       $(this).siblings(".active").removeClass("active")
       $(this).addClass("active")
